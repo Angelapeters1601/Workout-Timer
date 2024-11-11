e138d058cc45c25896f690053d4790cb574a627f
Angelapeters1601 <nwattaha@gmail.com>
2024-11-11 01:21:30 +0100

    Added state for duration, then synced it with other states by updating it using effect

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,4 +1,4 @@
-import { useState, memo, useCallback, useMemo } from "react";
+import { useState, useEffect, memo, useCallback } from "react";
 import clickSound from "./ClickSound.m4a";
 
 const Calculator = function Calculator({ workouts, allowSound }) {
@@ -6,8 +6,13 @@ const Calculator = function Calculator({ workouts, allowSound }) {
   const [sets, setSets] = useState(3);
   const [speed, setSpeed] = useState(90);
   const [durationBreak, setDurationBreak] = useState(5);
+  const [duration, setDuration] = useState(0);
 
-  const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+  useEffect(() => {
+    setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+  }, [number, sets, speed, durationBreak]);
+
+  //const duration =(number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
 
@@ -16,6 +21,14 @@ const Calculator = function Calculator({ workouts, allowSound }) {
     const sound = new Audio(clickSound);
     sound.play();
   }, [allowSound]);
+
+  const handleInc = () => {
+    setDuration((duration) => Math.floor(duration) + 1);
+  };
+
+  const handleDec = () => {
+    setDuration((duration) => (duration > 1 ? Math.ceil(duration) - 1 : 0));
+  };
 
   return (
     <>
@@ -66,13 +79,13 @@ const Calculator = function Calculator({ workouts, allowSound }) {
         </div>
       </form>
       <section>
-        <button onClick={() => {}}>–</button>
+        <button onClick={handleDec}>–</button>
         <p>
           {mins < 10 && "0"}
           {mins}:{seconds < 10 && "0"}
           {seconds}
         </p>
-        <button onClick={() => {}}>+</button>
+        <button onClick={handleInc}>+</button>
       </section>
     </>
   );
